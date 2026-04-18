03ec6377504740da5dc2a7efb39fddde07cd882f
Shashika Dias <shashikadias09@gmail.com>
2026-04-18 21:29:05 +0530

    add_delivered_at_column.sql

--- TestTEst/vehicle-final/database/add_delivered_at_column.sql
+++ TestTEst/vehicle-final/database/add_delivered_at_column.sql
@@ -1,7 +1,6 @@
 -- Add delivered_at column to orders table
 ALTER TABLE orders ADD COLUMN delivered_at DATETIME AFTER approved_at;
 
--- Update existing DELIVERED orders with a timestamp
 UPDATE orders 
 SET delivered_at = NOW() 
 WHERE status = 'DELIVERED' AND delivered_at IS NULL;
