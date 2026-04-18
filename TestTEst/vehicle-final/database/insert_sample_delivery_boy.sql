7f7e3e13a9cdd03f358397506159420ed4a1737c
Shashika Dias <shashikadias09@gmail.com>
2026-04-18 21:35:41 +0530

    insert_sample_delivery_boy.sql

--- TestTEst/vehicle-final/database/insert_sample_delivery_boy.sql
+++ TestTEst/vehicle-final/database/insert_sample_delivery_boy.sql
@@ -5,6 +5,7 @@ INSERT INTO users (username, email, password, full_name, phone, address, role_id
 -- Get the user ID of the inserted user
 SET @delivery_boy_id = LAST_INSERT_ID();
 
+
 -- Insert delivery boy details
 INSERT INTO delivery_boy_details (user_id, vehicle_make, vehicle_model, vehicle_number, license_number, driving_experience) VALUES
 (@delivery_boy_id, 'Toyota', 'Corolla', 'SAM123', 'DL1234567890', 5);
